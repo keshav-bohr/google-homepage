77d92cd03d1032df725a675de6f2bc10e9101015
Keshav Bohra <keshav2016@gmail.com>
2020-06-29 01:34:26 +0530

    added navbar and basic css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import './navbar.scss'
 
 function navbar(props) {
     return (
         <div className="navbar">
             <div className="navbar-item-container">
-
+                <div className="navbar-item">
+                    <a href="https://mail.google.com/mail/?tab=wm&authuser=0&ogbl">Gmail</a>
+                </div>
+                <div className="navbar-item">
+                    <a href="https://www.google.co.in/imghp?hl=en&tab=wi&authuser=0&ogbl">Images</a>
+                </div>
             </div>
             <div className="navbar-item-container">
-
+                <div className="navbar-item">
+                    <a href="#">Apps</a>
+                </div>
+                <div className="navbar-item">
+                    <a href="#">Avatar</a>
+                </div>
             </div>
         </div>
     )
