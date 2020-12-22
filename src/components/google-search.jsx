ba949d6037c09f12362dc8790f7b4f1c5a961bfa
Keshav Bohra <keshav2016@gmail.com>
2020-12-22 17:43:27 +0530

    added google logo

---
+++ src/components/google-search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import googleLogo from '../assets/google-logo.png'
+
+function googleSearch(props) {
+    return (
+        <div>
+            <img src={googleLogo} />
+        </div>
+    )
+}
+
+
+export default googleSearch
+
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./navbar";
+import GoogleSearch from './google-search.jsx'
 
 function homepage(props) {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <GoogleSearch />
+        </>
     )
 }
 
