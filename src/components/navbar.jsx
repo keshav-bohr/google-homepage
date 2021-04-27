489b09f335f1df4b719058a2720395ac97ffb478
Keshav Bohra <keshav2016@gmail.com>
2021-04-27 20:38:49 +0530

    added styling to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import './navbar.scss'
+import './navbar.scss';
+import Avatar from '@material-ui/core/Avatar';
+import AppsIcon from '@material-ui/icons/Apps';
 
 function navbar(props) {
     return (
         <div className="navbar">
-            <div className="navbar-item-container">
+            <div className="navbar-item-container-wrapper">
+                <div className="navbar-item-container">
+                    <div className="navbar-item">
+                        <a href="https://mail.google.com/mail/?tab=wm&authuser=0&ogbl">Gmail</a>
+                    </div>
+                    <div className="navbar-item">
+                        <a href="https://www.google.co.in/imghp?hl=en&tab=wi&authuser=0&ogbl">Images</a>
+                    </div>
+                </div>
+                <div className="navbar-item-container">
                 <div className="navbar-item">
-                    <a href="https://mail.google.com/mail/?tab=wm&authuser=0&ogbl">Gmail</a>
+                    {/* <a href="#">Apps</a> */}
+                    <AppsIcon fontSize="medium"/>
                 </div>
                 <div className="navbar-item">
-                    <a href="https://www.google.co.in/imghp?hl=en&tab=wi&authuser=0&ogbl">Images</a>
+                    <Avatar className="navbar-avatar">K</Avatar>
                 </div>
             </div>
-            <div className="navbar-item-container">
-                <div className="navbar-item">
-                    <a href="#">Apps</a>
-                </div>
-                <div className="navbar-item">
-                    <a href="#">Avatar</a>
-                </div>
             </div>
         </div>
     )
