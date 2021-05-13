a1394e9866d377141163053a4b60cea231052a7d
Keshav Bohra <keshav2016@gmail.com>
2021-05-14 00:34:27 +0530

    added other google apps wrapper

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,26 +5,74 @@ import AppsIcon from '@material-ui/icons/Apps';
 
 function navbar(props) {
     return (
-        <div className="navbar">
-            <div className="navbar-item-container-wrapper">
-                <div className="navbar-item-container">
+        <>
+            <div className="navbar">
+                <div className="navbar-item-container-wrapper">
+                    <div className="navbar-item-container">
+                        <div className="navbar-item">
+                            <a href="https://mail.google.com/mail/?tab=wm&authuser=0&ogbl">Gmail</a>
+                        </div>
+                        <div className="navbar-item">
+                            <a href="https://www.google.co.in/imghp?hl=en&tab=wi&authuser=0&ogbl">Images</a>
+                        </div>
+                    </div>
+                    <div className="navbar-item-container">
                     <div className="navbar-item">
-                        <a href="https://mail.google.com/mail/?tab=wm&authuser=0&ogbl">Gmail</a>
+                        {/* <a href="#">Apps</a> */}
+                        <AppsIcon fontSize="medium"/>
                     </div>
                     <div className="navbar-item">
-                        <a href="https://www.google.co.in/imghp?hl=en&tab=wi&authuser=0&ogbl">Images</a>
+                        <Avatar className="navbar-avatar">K</Avatar>
                     </div>
                 </div>
-                <div className="navbar-item-container">
-                <div className="navbar-item">
-                    {/* <a href="#">Apps</a> */}
-                    <AppsIcon fontSize="medium"/>
-                </div>
-                <div className="navbar-item">
-                    <Avatar className="navbar-avatar">K</Avatar>
                 </div>
             </div>
-            </div>
+            {otherGoogleApps()}
+        </>
+    )
+}
+
+function otherGoogleApps() {
+    return (
+        <div className='other-google-apps-wrapper'>
+            <ul className='other-google-apps-list'>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+                <li className='other-google-apps-list-icon'>
+
+                </li>
+            </ul>
         </div>
     )
 }
