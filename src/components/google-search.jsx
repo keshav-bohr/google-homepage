4bb00efd447912f2232c73432ddedfdd0c849277
Keshav Bohra <keshav2016@gmail.com>
2021-06-22 14:33:07 +0530

    added basic form

--- src/components/google-search.jsx
+++ src/components/google-search.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import googleLogo from '../assets/google-logo.png'
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import MicIcon from '@material-ui/icons/Mic';
 import './google-search.scss'
 
 function googleSearch(props) {
     return (
-        <div className='google-logo'>
-            <img src={googleLogo} alt="google-logo"/>
+        <div className='main-content'>
+            <div className='google-logo'>
+                <img src={googleLogo} alt="google-logo"/>
+            </div>
+            <form noValidate>
+                <div>
+                    <SearchOutlinedIcon />
+                    <input className='search-input'/>
+                    <MicIcon />
+                </div>
+            </form>
         </div>
     )
 }
