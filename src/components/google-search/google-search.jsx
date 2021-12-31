e513ac9fad62248582d87fc40e756169f476615a
Keshav Bohra <keshav2016@gmail.com>
2021-12-31 13:41:09 +0530

    added google search buttons with css

--- src/components/google-search/google-search.jsx
+++ src/components/google-search/google-search.jsx
@@ -1,46 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import googleLogo from '../../assets/google-logo.png'
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import MicIcon from '@material-ui/icons/Mic';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import './google-search.scss'
 
-const searchHistory = [
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'},
-    {title: 'history'}
-]
 
 function GoogleSearch(props) {
     const [searchHistory, setSearchHistory] = useState([])
+    const [search, setSearch] = useState("")
     
     return (
         <div className='main-content'>
             <div className='google-logo'>
                 <img src={googleLogo} alt="google-logo"/>
             </div>
-            <form noValidate className='search-form'>
-                <div className="search-bar-wrapper">
+            <form noValidate className='search-form' onSubmit={(e) => {
+                e.preventDefault()
+                if(search.trim() !== ""){
+                    setSearchHistory([...searchHistory, {title: search}])
+                    setSearch("")
+                }
+            }}>
+                <div 
+                // className='search-bar-wrapper'>
+                className={`search-bar-wrapper ${searchHistory.length ? "search-bar-wrapper-with-history": ""}`}>
                     <SearchOutlinedIcon />
-                    <input className='search-input'/>
+                    <input className='search-input' onChange={e => { setSearch(e.target.value) }} value={search}/>
                     <MicIcon />
                 </div>
-                <div className='google-search-buttons-container'>
-                    <div className='google-search-button' type='button'>Google Search</div>
-                    <div className='google-search-button' type='button'>I'm Feeling Lucky</div>
-                </div>
-                {/* <div className="search-history-wrapper">
+                
+                {searchHistory.length ? <div className="search-history-wrapper">
                     <div className="search-partition"></div>
                     <ul className="search-history-list">
                         {searchHistory.map((singleItem) => {
-                            return <li className="search-history-item">
+                            return <li key={singleItem.title} className="search-history-item">
                                 <ScheduleIcon fontSize='small'/>
                                 <div className='search-history-item-description'>
                                     <div className='search-history-item-title'>
@@ -53,7 +47,11 @@ function GoogleSearch(props) {
                             </li>
                         })}
                     </ul>
-                </div> */}
+                </div> : null}
+                <div className='google-search-buttons-container'>
+                    <input className='google-search-button' type='submit' value="Google Search" />
+                    <input className='google-search-button' type='button' value="I'm Feeling Lucky"/>
+                </div>
             </form>
         </div>
     )
