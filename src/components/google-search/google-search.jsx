5f26d9a02f5f2ee61b4de8e4d5aa0df7d2fee7ce
Keshav Bohra <keshav2016@gmail.com>
2022-07-22 23:13:34 +0530

    formatting the google search component

--- src/components/google-search/google-search.jsx
+++ src/components/google-search/google-search.jsx
@@ -9,33 +9,33 @@ import './google-search.scss'
 function GoogleSearch(props) {
     const [searchHistory, setSearchHistory] = useState([])
     const [search, setSearch] = useState("")
-    
+
     return (
         <div className='main-content'>
             <div className='google-logo'>
-                <img src={googleLogo} alt="google-logo"/>
+                <img src={googleLogo} alt="google-logo" />
             </div>
             <form noValidate className='search-form' onSubmit={(e) => {
                 e.preventDefault()
-                if(search.trim() !== ""){
-                    setSearchHistory([...searchHistory, {title: search}])
+                if (search.trim() !== "") {
+                    setSearchHistory([...searchHistory, { title: search }])
                     setSearch("")
                 }
             }}>
-                <div 
-                // className='search-bar-wrapper'>
-                className={`search-bar-wrapper ${searchHistory.length ? "search-bar-wrapper-with-history": ""}`}>
+                <div
+                    // className='search-bar-wrapper'>
+                    className={`search-bar-wrapper ${searchHistory.length ? "search-bar-wrapper-with-history" : ""}`}>
                     <SearchOutlinedIcon />
-                    <input className='search-input' onChange={e => { setSearch(e.target.value) }} value={search}/>
+                    <input className='search-input' onChange={e => { setSearch(e.target.value) }} value={search} />
                     <MicIcon />
                 </div>
-                
+
                 {searchHistory.length ? <div className="search-history-wrapper">
                     <div className="search-partition"></div>
                     <ul className="search-history-list">
                         {searchHistory.map((singleItem) => {
                             return <li key={singleItem.title} className="search-history-item">
-                                <ScheduleIcon fontSize='small'/>
+                                <ScheduleIcon fontSize='small' />
                                 <div className='search-history-item-description'>
                                     <div className='search-history-item-title'>
                                         <span className='search-history-title'>{singleItem.title}</span>
@@ -50,7 +50,7 @@ function GoogleSearch(props) {
                 </div> : null}
                 <div className='google-search-buttons-container'>
                     <input className='google-search-button' type='submit' value="Google Search" />
-                    <input className='google-search-button' type='button' value="I'm Feeling Lucky"/>
+                    <input className='google-search-button' type='button' value="I'm Feeling Lucky" />
                 </div>
             </form>
         </div>
